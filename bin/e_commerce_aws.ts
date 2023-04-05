a0f5bc02390131772a82cf5b643bc0b0f7832710
Jhonatan Sena <jcostasenna@hotmail.com>
2023-04-05 15:25:20 -0300

    feat: organization stacks

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import { ProductsAppStack } from '../lib/productsApp-stack';
+import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
+
 
 const app = new cdk.App();
+
+const env: cdk.Environment = {
+  account: "001050166178",
+  region: "us-east-1"
+}
+
+const tags = {
+  cost: "ECommerce",
+  team: "SenaCode"
+}
+
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
+  tags: tags,
+  env: env
+})
+
+const eCommerceApiStack = new EcommerceApiStack(app, "ECommerceApi", {
+  productsFetchHandler: productsAppStack.productsFetchHandler,
+  tags: tags,
+  env: env
+})
+
+eCommerceApiStack.addDependency(productsAppStack)
