9753977fc1022f26ca8dac4f9aa691501c703a72
Jhonatan Sena <jcostasenna@hotmail.com>
2023-04-07 20:34:58 -0300

    feat: create a dynamodb table from product

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -2,12 +2,26 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 import { Construct } from "constructs"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly productsDb: dynamodb.Table
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
+
+        this.productsDb = new dynamodb.Table(this, "ProductsDdb", {
+            tableName: "products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: "id",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
 
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
             "ProductsFetchHandler", {
