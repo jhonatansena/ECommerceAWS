f81bcd0c11736833416eebfa95a85cad594456fd
Jhonatan Sena <jcostasenna@hotmail.com>
2023-04-07 21:11:31 -0300

    feat: add the handle absent from admin products

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -24,6 +24,7 @@ const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
 
 const eCommerceApiStack = new EcommerceApiStack(app, "ECommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
+  productsAdminHandler: productsAppStack.productsAdminHandler,
   tags: tags,
   env: env
 })
