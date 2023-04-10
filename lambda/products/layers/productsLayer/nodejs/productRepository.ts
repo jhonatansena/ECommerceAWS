a39c7958747e8f97b9f490f4375857b4008da346
Jhonatan Sena <jcostasenna@hotmail.com>
2023-04-09 22:02:03 -0300

    feat: add list products on repository

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,7 +1,28 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { v4 as uuid } from "uuid"
+
 export interface Product {
     id: string;
     productName: string;
     code: string;
     price: number;
     model: string
+}
+
+export class ProductRepository {
+    private dynamoDbClient: DocumentClient
+    private productsDdb: string
+
+    constructor(dynamoDbClient: DocumentClient, producsDdb: string) {
+        this.dynamoDbClient = dynamoDbClient
+        this.productsDdb = producsDdb   
+    }
+
+    async getAllProducts(): Promise<Product[]> {
+      const data = await this.dynamoDbClient.scan({
+            TableName: this.productsDdb
+        }).promise()
+
+        return data.Items as Product[]
+    }
 }
