4f9b7452a308f3a57e96d245fb3a859fd047b93a
Jhonatan Sena <jcostasenna@hotmail.com>
2023-04-11 21:26:15 -0300

    feat: add lambda layer on products admin function

--- lambda/products/productsAdminFunctions.ts
+++ lambda/products/productsAdminFunctions.ts
@@ -1,4 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { DynamoDB } from 'aws-sdk'
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer";
+
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient()
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler(event: APIGatewayProxyEvent, 
     context: Context): Promise<APIGatewayProxyResult> {
@@ -13,12 +21,11 @@ export async function handler(event: APIGatewayProxyEvent,
         if (event.resource === "/products") {
             console.log('POST /products')
             code = 201
-
+            const product = JSON.parse(event.body!) as Product
+            const productCreated = await productRepository.create(product)
             return {
                 statusCode: code,
-                body: JSON.stringify({
-                    message: "POST /products - OK"
-                })
+                body: JSON.stringify(productCreated)
             }
         }
 
@@ -27,21 +34,37 @@ export async function handler(event: APIGatewayProxyEvent,
             let message
             
             if(method === 'PUT') {
-             message = "PUT /products/{id} - OK"
-             code = 204
+            const product = JSON.parse(event.body!)
+
+            try {
+                const productUpdated = await productRepository.updateProduct(productId, product)
+                message = productUpdated
+                code = 204
+            } catch (CondicionalCheckException) {
+                return {
+                    statusCode: 400,
+                    body: 'Product not found'
+                }
+            }
             } 
             
             if (method === 'DELETE') {
-              message = "DELTE /products/{id} - OK"
-              code = 200  
+             try {
+                const product = await productRepository.deleteProduct(productId)
+                message = product
+                code = 200  
+             } catch (error) {
+                console.error((<Error>error).message)
+                return {
+                    statusCode: 400,
+                    body: (<Error>error).message
+                }
+             }
             }
-
             
             return {
                 statusCode: code,
-                body: JSON.stringify({
-                    message
-                })
+                body: JSON.stringify(message)
             }
         }
 
